4c79a6d2b48b004fbdc266c84965060a78200841
roshen1903 <rovsenbekirov@icloud.com>
2024-12-25 22:23:39 +0400

    Delete app directory

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from app.controllers.job_controller import job_blueprint
-
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(job_blueprint)
-    return app
